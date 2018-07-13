7ba93df3e7bda269fefc26634e359b332eba14f6
Cigan1234 <djordje_96@hotmail.com>
2018-07-14 00:49:43 +0200

    initial commit

---
+++ betOrDie/src/main/java/com/djordje/betOrDie/betOrDie/BetOrDieApplication.java
@@ -0,0 +1,12 @@
+package com.djordje.betOrDie.betOrDie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetOrDieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetOrDieApplication.class, args);
+	}
+}
